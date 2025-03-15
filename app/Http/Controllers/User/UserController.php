50ba7643dcc8eacf9515ec2bd13ab0959fa434a9
Rianndd <riandroshevara23@gmail.com>
2025-03-16 00:43:32 +0700

    update: jika users role siswa tambah input kelas

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,19 +4,16 @@ namespace App\Http\Controllers\User;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use App\Http\Controllers\Controller; 
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
-
-
-
+use App\Http\Controllers\Controller;
+use App\Models\Kelas;
 
 class UserController extends Controller
 {
     public function index()
     {
         $users = User::all();
-        return view('user.index', compact('users'));
+        $kelas = Kelas::all();
+        return view('user.index', compact('users', 'kelas'));
     }
 
     public function create()
@@ -25,48 +22,47 @@ class UserController extends Controller
     }
 
     public function store(Request $request)
-{
-    $validator = \Validator::make($request->all(), [
-        'name' => 'required',
-        'username' => 'required',
-        'email' => 'required',
-        'password' => 'required|min:6',
-        'role' => ['required', Rule::in(['guru', 'admin', 'siswa'])],
-    ]);
+    {
+        $request->validate([
+            'name' => 'required',
+            'username' => 'required',
+            'id_kelas' => 'nullable',
+            'email' => 'required',
+            'password' => 'required',
+            'role' => 'required',
+        ]);
 
-   // if ($validator->fails()) {
-     //   return response()->json(['success' => false, 'errors' => $validator->errors()], 422);
-    //}
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'id_kelas' => $request->id_kelas ?? null,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+        ]);
 
-    $user = User::create([
-        'name' => $request->name,
-        'username' => $request->username,
-        'email' => $request->email,
-        'password' => Hash::make($request->password),
-        'role' => $request->role,
-    ]);
+        return redirect()->route('user.index')->with('success', 'Data berhasil disimpan');
+    }
 
-   // return response()->json(['success' => true, 'message' => 'User berhasil ditambahkan']);
-    return redirect()->route('user.index')->with('success', 'Data berhasil disimpan');
-}
     public function edit(string $id)
     {
         $user = User::findOrFail($id);
         //return view('user.edit', compact('user'));
     }
+
     public function update(Request $request, User $user)
-{
-    $request->validate([
-        'name' => 'required',
-        'username' => 'required|unique:users,username,' . $user->id,
-        'email' => 'required|email|unique:users,email,' . $user->id,
-        'role' => 'required|in:guru,admin,siswa',
-    ]);
+    {
+        $request->validate([
+            'name' => 'required',
+            'username' => 'required|unique:users,username,' . $user->id,
+            'email' => 'required|email|unique:users,email,' . $user->id,
+            'role' => 'required|in:guru,admin,siswa',
+        ]);
 
-    $user->update($request->all());
+        $user->update($request->all());
 
-    return redirect()->route('user.index')->with('success', 'User berhasil diperbarui');
-}
+        return redirect()->route('user.index')->with('success', 'User berhasil diperbarui');
+    }
 
     public function destroy($id)
     {
